9a2a97602b3f9af93cec9dc17f19ecd3f41d197d
Martin Tang <martin.tang@oaknorth.ai>
2021-02-22 22:37:03 +0000

    Update bevy and add steps from "getting started" section in book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+//! Hello
+
+use bevy::prelude::*;
+
+struct Person;
+struct Name(String);
+
+fn add_ppl(commands: &mut Commands) {
+    commands
+        .spawn((Person, Name("Elaina Proctor".into())))
+        .spawn((Person, Name("Renzo Hume".into())))
+        .spawn((Person, Name("Zayna Nieves".into())));
+}
+
+struct GreetTimer(Timer);
+fn greet_ppl(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if !timer.0.tick(time.delta_seconds()).just_finished() {
+        return;
+    }
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_ppl.system())
+            .add_system(greet_ppl.system());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
 }
